ca7da5c5cfd275f572a90d31e51a726b25449d7d
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-03-09 22:46:55 +0530

    update server js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+const vehicalRouts = require('./routes/vehical');
+const bookingRoutes = require('./routes/booking');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api',authRoutes);
+app.use('/api/vehical',vehicalRouts);
+app.use('/api/booking',bookingRoutes);
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port 3000`);
+});
